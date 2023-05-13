d66fd547dc43cfec4d2dea6356b6f94e636071b5
Christian Salvadora <christianrsalvadora@gmail.com>
2023-05-13 16:18:23 +0800

    Installed react router

--- src/StudentWall_frontend/src/index.jsx
+++ src/StudentWall_frontend/src/index.jsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { StudentWall_backend as custom_greeting } from "../../declarations/StudentWall_backend";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const MyHello = () => {
-  const [name, setName] = React.useState('');
-  const [message, setMessage] = React.useState('');
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
-  async function doGreet() {
-    const greeting = await custom_greeting.greet(name);
-    setMessage(greeting);
-  }
-
-  return (
-    <div className="col-10">hello</div>
-  );
-};
-
-render(<MyHello />, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("app")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

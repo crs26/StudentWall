a15a341d974b63bbe052222beb15e9d7065f86ea
Christian Salvadora <christianrsalvadora@gmail.com>
2023-05-13 15:59:56 +0800

    Installed bootstrap

--- src/StudentWall_frontend/src/index.jsx
+++ src/StudentWall_frontend/src/index.jsx
@@ -12,28 +12,7 @@ const MyHello = () => {
   }
 
   return (
-    <div style={{ "fontSize": "30px" }}>
-      <div style={{ "backgroundColor": "yellow" }}>
-        <p>Greetings, from DFINITY!</p>
-        <p>
-          {" "}
-          Type your message in the Name input field, then click{" "}
-          <b> Get Greeting</b> to display the result.
-        </p>
-      </div>
-      <div style={{ margin: "30px" }}>
-        <input
-          id="name"
-          value={name}
-          onChange={(ev) => setName(ev.target.value)}
-        ></input>
-        <button onClick={doGreet}>Get Greeting!</button>
-      </div>
-      <div>
-        Greeting is: "
-        <span style={{ color: "blue" }}>{message}</span>"
-      </div>
-    </div>
+    <div className="col-10">hello</div>
   );
 };
 

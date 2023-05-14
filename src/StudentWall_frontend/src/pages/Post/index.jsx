7d4858b5cdcda059bc9ecf78c43c9ba0d85c8f5b
pseudoman21 <johnpaul.batusa@gmail.com>
2023-05-14 22:27:23 +0800

    Connect post page on the backend

--- src/StudentWall_frontend/src/pages/Post/index.jsx
+++ src/StudentWall_frontend/src/pages/Post/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NewPostCard from '../../components/NewPostCard'
 import PostCard from '../../components/PostCard'
+import { StudentWall_backend as backend } from "../../../../declarations/StudentWall_backend";
+
 
 export const Post = () => {
+  const [posts, setPosts] = useState([{}])
+  useEffect(() => {
+    backend.getAllMessages().then((posts) => {
+      setPosts(posts)
+    })
+  }, []);
+
   return (
     <div className="container justify-content-center">
       <NewPostCard />
-      <PostCard />
+      {
+        posts?.map((post, id) => {
+          return <PostCard key={id} id={id} data={post} />
+        })
+      }
     </div>
   )
 }

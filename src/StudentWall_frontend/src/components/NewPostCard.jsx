e0564cc86f7820873531338d46feba32e03cc7a6
pseudoman21 <johnpaul.batusa@gmail.com>
2023-05-14 22:25:54 +0800

    Update new post card

--- src/StudentWall_frontend/src/components/NewPostCard.jsx
+++ src/StudentWall_frontend/src/components/NewPostCard.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { StudentWall_backend as backend } from '../../../declarations/StudentWall_backend';
 
 export default function NewPostCard() {
+  const [post, setPost] = useState({});
+
+  const createPost = async () => {
+
+  }
+  useEffect(() => {
+    console.log(post)
+  }, [post]);
   return (
     <div className='d-flex post-card my-3'>
       <img src='/user.png' className='user-img my-auto' />
-      <textarea placeholder='Share something on your mind' className='mx-2'></textarea>
-      <button className='primary-btn'>Create Post</button>
+      <div className='d-grid form-inputs'>
+        <input name='subject' type='text' placeholder='Pick a topic' className='mb-2' onChange={(e) => setPost({ ...post, subject: e.target.value })} />
+        <textarea placeholder='Share something on your mind'
+          onChange={(e) => {
+            setPost({ ...post, text: e.target.value })
+          }}
+        ></textarea>
+      </div>
+      <button className='primary-btn my-auto'>Create Post</button>
     </div>
   )
 }

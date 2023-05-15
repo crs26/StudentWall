30f441e29d5b0841e5347e234a03997f879a148d
pseudoman21 <johnpaul.batusa@gmail.com>
2023-05-15 22:29:56 +0800

    Update new post card layout

--- src/StudentWall_frontend/src/components/NewPostCard.jsx
+++ src/StudentWall_frontend/src/components/NewPostCard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useAuth } from '../helpers/use-auth-client'
 import { toast } from 'react-toastify'
 
-export default function NewPostCard ({ subject, body, edit, id, setEditPost, update, setData }) {
+export default function NewPostCard({ subject, body, edit, id, setEditPost, update, setData }) {
   const { whoamiActor } = useAuth()
 
   const [post, setPost] = useState({
@@ -29,12 +29,15 @@ export default function NewPostCard ({ subject, body, edit, id, setEditPost, upd
   }, [alert.status])
 
   const createPost = () => {
-    if (post?.text !== '' && post?.subject !== '') {
+    console.log('here')
+    if (typeof post?.text !== 'undefined' && typeof post?.subject !== 'undefined') {
       whoamiActor.writeMessage(post?.subject, { Text: post?.text }).then((result) => {
-        toast('Post has been uploaded')
+        toast('Post has been created')
         update()
+        setPost({ subject: '', text: '' })
       })
     } else {
+      toast('Cannot create empty post')
       setAlert({ ...alert, message: 'Cannot add an empty post!', status: false })
     }
   }
@@ -46,6 +49,7 @@ export default function NewPostCard ({ subject, body, edit, id, setEditPost, upd
         update()
       })
     } else {
+      toast('Cannot create empty post')
       setAlert({ ...alert, message: 'Cannot add an empty post!', status: false })
     }
   }
@@ -61,11 +65,11 @@ export default function NewPostCard ({ subject, body, edit, id, setEditPost, upd
   return (
 
     <div className='d-md-flex post-card my-3'>
-      <div className='d-flex w-100 gap-md-4 justify-content-between justify-content-md-start'>
-        <div className='col-1 my-auto d-flex'>
-          <img src='/user.png' className='user-img m-auto' />
+      <div className='d-flex w-100 justify-content-between justify-content-md-start'>
+        <div className='col-1 mx-auto'>
+          <img src='/user.png' className='d-flex user-img mx-auto' />
         </div>
-        <div className='col-10 col-md-10 d-grid form-inputs'>
+        <div className='col-10 col-md-11 d-grid form-inputs'>
           <input name='subject' type='text' placeholder='Pick a topic' className='mb-2' onChange={(e) => setPost({ ...post, subject: e.target.value })} value={post?.subject} />
           <textarea
             placeholder='Share something on your mind'
@@ -74,10 +78,10 @@ export default function NewPostCard ({ subject, body, edit, id, setEditPost, upd
               setPost({ ...post, text: e.target.value })
             }}
           />
+          <div className='mx-auto col-12 col-md-3 col-lg-2 justify-content-end mt-3 d-flex w-100'>
+            <button className='primary-btn mt-2 my-md-auto' onClick={handlePost}>{!edit ? 'Create Post' : 'Edit Post'}</button>
+          </div>
         </div>
-      </div>
-      <div className='mx-auto col-12 col-md-3 col-lg-2 d-flex'>
-        <button className='primary-btn mt-2 my-md-auto w-100 px-0' onClick={handlePost}>{!edit ? 'Create Post' : 'Edit Post'}</button>
       </div>
     </div>
   )
